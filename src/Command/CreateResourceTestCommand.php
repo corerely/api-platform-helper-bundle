50f9aea124dff854f3200c30369fcf1f31a0d2c6
Andrii Oprysko <anjey.link@gmail.com>
2022-11-24 21:19:51 +0200

    fix phpcs

--- src/Command/CreateResourceTestCommand.php
+++ src/Command/CreateResourceTestCommand.php
@@ -7,8 +7,6 @@ use ApiPlatform\Metadata\Resource\Factory\ResourceNameCollectionFactoryInterface
 use ApiPlatform\Util\Inflector;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\Input;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
