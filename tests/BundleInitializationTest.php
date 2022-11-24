89f2c78423b6bf48032921212864b4e713b505f2
Andrii Oprysko <anjey.link@gmail.com>
2022-11-24 20:07:45 +0200

    change assert call

--- tests/BundleInitializationTest.php
+++ tests/BundleInitializationTest.php
@@ -25,34 +25,34 @@ class BundleInitializationTest extends KernelTestCase
 
     public function testPermanentFilterExtensionService(): void
     {
-        $this->assertServiceConfigured('corerely.api_platform_helper.doctrine.permanent_filter_extension', PermanentFilterExtension::class);
+        self::assertServiceConfigured('corerely.api_platform_helper.doctrine.permanent_filter_extension', PermanentFilterExtension::class);
     }
 
     public function testIdentifierCollectionFilterExtensionService(): void
     {
-        $this->assertServiceConfigured('corerely.api_platform_helper.doctrine.identifier_collection_filter_extension', IdentifierCollectionFilterExtension::class);
+        self::assertServiceConfigured('corerely.api_platform_helper.doctrine.identifier_collection_filter_extension', IdentifierCollectionFilterExtension::class);
     }
 
     public function testOrderByFieldsExtensionService(): void
     {
-        $this->assertServiceConfigured('corerely.api_platform_helper.doctrine.order_by_fields_extension', OrderByFieldsExtension::class);
+        self::assertServiceConfigured('corerely.api_platform_helper.doctrine.order_by_fields_extension', OrderByFieldsExtension::class);
     }
 
     public function testTextSearchFilterService(): void
     {
-        $this->assertServiceConfigured('corerely.api_platform_helper.doctrine.text_search_filter', TextSearchFilter::class);
+        self::assertServiceConfigured('corerely.api_platform_helper.doctrine.text_search_filter', TextSearchFilter::class);
     }
 
     public function testUuidFilterService(): void
     {
-        $this->assertServiceConfigured('corerely.api_platform_helper.doctrine.uuid_filter', UuidFilter::class);
+        self::assertServiceConfigured('corerely.api_platform_helper.doctrine.uuid_filter', UuidFilter::class);
     }
 
     private function assertServiceConfigured(string $id, string $className): void
     {
-        $this->assertTrue($this->testContainer->has($id));
+        self::assertTrue($this->testContainer->has($id));
 
         $service = $this->testContainer->get($id);
-        $this->assertInstanceOf($className, $service);
+        self::assertInstanceOf($className, $service);
     }
 }
